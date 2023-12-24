375d9904f4f3227bbc143b71783b9bbf7acc4ad7
Shubham Lal <im.shubhamlal@gmail.com>
2023-12-24 16:09:41 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const websiteButton = document.querySelector('.share-button');
+const shareButtons = document.querySelectorAll('.tile-share-button');
+
+async function copyText(e) {
+    e.preventDefault();
+
+    const link = this.getAttribute('link');
+
+    try {
+        await navigator.clipboard.writeText(link);
+        alert("Copied to clipboard!");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+websiteButton.addEventListener('click', copyText);
+shareButtons.forEach(shareButton => shareButton.addEventListener('click', copyText));
